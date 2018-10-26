04500a8791778034d80e9fbfb571996d9f2b5c03
LamarqueAlexandre <lamarque.alexandre7@gmail.com>
2018-10-26 06:15:42 -0700

    Updated files

--- 13-pizzastore/pizza_add.php
+++ 13-pizzastore/pizza_add.php
@@ -1,50 +1,139 @@
 <?php
-// Inclus la base de données
-require_once(__DIR__.'/config/database.php');
-
+$currentPageTitle = 'Ajouter une pizza';
 // Le fichier header.php est inclus sur la page
-require_once(__DIR__.'/partials/header.php'); ?>
+require_once(__DIR__.'/partials/header.php');
+// Traitement du formulaire
+$name = $price = $image = $category = $description = null;
+// le formulaire est soumis
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $price = str_replace(',', '.', $_POST['price']); // on remplace la , par un . pour le prix
+    $image = $_POST['image'];
+    $category = $_POST['category'];
+    $description = $_POST['description'];
+    // Raccourci avec l'interpolation de variables
+    // ${'variable'} = 'valeur';
+    // $key = 'variable';
+    // ${$key} = 'valeur';
+    // foreach ($_POST as $key => $field) {
+    //    $$key = $field;
+    // }
+    // Définir un tableau d'erreur vide qui va se remplir après chaque erreur
+    $errors = [];
+    // Vérifier le name
+    if (empty($name)) {
+        $errors['name'] = 'Le nom n\'est pas valide';
+    }
+    // Vérifier le price
+    if (!is_numeric($price) || $price < 5 || $price > 19.99) {
+        $errors['price'] = 'Le prix n\'est pas valide';
+    }
+    // Vérifier l'image
+    if (empty($image)) {
+        $errors['image'] = 'L\'image n\'est pas valide';
+    }
+    // Vérifier la catégorie
+    if (empty($category) || !in_array($category, ['Classique', 'Spicy', 'Hot', 'Végétarienne'])) {
+        $errors['category'] = 'La catégorie n\'est pas valide';
+    }
+    // Vérifier la description
+    if (strlen($description) < 10) {
+        $errors['description'] = 'La description n\'est pas valide';
+    }
+    // S'il n'y a pas d'erreurs dans le formulaire
+    if (empty($errors)) {
+        $query = $db->prepare('
+            INSERT INTO pizza (`name`, `price`, `image`, `category`, `description`) VALUES (:name, :price, :image, :category, :description)
+        ');
+        $query->bindValue(':name', $name, PDO::PARAM_STR);
+        $query->bindValue(':price', $price, PDO::PARAM_STR);
+        $query->bindValue(':image', $image, PDO::PARAM_STR);
+        $query->bindValue(':category', $category, PDO::PARAM_STR);
+        $query->bindValue(':description', $description, PDO::PARAM_STR);
+        if ($query->execute()) { // On insère la pizza dans la BDD
+            $success = true;
+            // Envoyer un mail ?
+            // Logger la création de la pizza
+        }
+    }
+}
+?>
 
-    <main class="container">
-        <h1>Ajouter une pizza</h1>
+<main class="container">
+    <h1 class="page-title">Ajouter une pizza</h1>
 
+    <?php if (isset($success) && $success) { ?>
+        <div class="alert alert-success alert-dismissible fade show">
+            La pizza <strong><?php echo $name; ?></strong> a bien été ajouté avec l'id <strong><?php echo $db->lastInsertId(); ?></strong> !
+            <button type="button" class="close" data-dismiss="alert">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php } ?>
+    
+    <form method="POST">
         <div class="row">
-            <div class="offset-lg-2 col-lg-6">
-                <form method="POST" id="add_pizza">
-                    <div class="form-group">
-                        <label for="pizza_name">Nom de la pizza</label>
-                        <input name="pizza_name" type="text" class="form-control" id="pizza_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="pizza_price">Prix</label>
-                        <input type="number" class="form-control" id="pizza_price">
-                    </div>
-                    <div class="form-group">
-                        <label for="img_pizza">Image</label>
-                        <input type="text" class="form-control" id="img_pizza">
-                    </div>
-                    <div class="form-group">
-                        <label class="pizza_describe">Description</label>
-                        <textarea class="form-control" form="add_pizza" rows="4"></textarea>
-                        <!-- <label for="pizza_describe">Description</label> -->
-                        <!-- <input type="text" class="form-control" id="pizza_describe"> -->
-                    </div>
-                    <div class="form-group">
-                        <select class="form-control" id="pizza_category">
-                            <option value="1">Base sauce tomate</option>
-                            <option value="2">Base crème fraîche</option>
-                            <option value="3">Base sauce BBQ</option>
-                        </select>
-                    </div>
-                    <div class="btn-submit">
-                        <button type="submit" class="btn btn-danger">Envoyer</button>
-                    </div>
-                </form>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="name">Nom :</label>
+                    <input type="text" name="name" id="name" class="form-control <?php echo isset($errors['name']) ? 'is-invalid' : null; ?>" value="<?php echo $name; ?>">
+                    <?php if (isset($errors['name'])) {
+                        echo '<div class="invalid-feedback">';
+                            echo $errors['name'];
+                        echo '</div>';
+                    } ?>
+                </div>
+                <div class="form-group">
+                    <label for="price">Prix :</label>
+                    <input type="text" name="price" id="price" class="form-control <?php echo isset($errors['price']) ? 'is-invalid' : null; ?>" value="<?php echo $price; ?>">
+                    <?php if (isset($errors['price'])) {
+                        echo '<div class="invalid-feedback">';
+                            echo $errors['price'];
+                        echo '</div>';
+                    } ?>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image :</label>
+                    <input type="text" name="image" id="image" class="form-control <?php echo isset($errors['image']) ? 'is-invalid' : null; ?>" value="<?php echo $image; ?>">
+                    <?php if (isset($errors['image'])) {
+                        echo '<div class="invalid-feedback">';
+                            echo $errors['image'];
+                        echo '</div>';
+                    } ?>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="category">Catégorie :</label>
+                    <select name="category" id="category" class="form-control <?php echo isset($errors['category']) ? 'is-invalid' : null; ?>">
+                        <option value="">Choisir la catégorie</option>
+                        <option <?php echo ($category === 'Classique') ? 'selected' : ''; ?> value="Classique">Classique</option>
+                        <option <?php echo ($category === 'Spicy') ? 'selected' : ''; ?> value="Spicy">Spicy</option>
+                        <option <?php echo ($category === 'Hot') ? 'selected' : ''; ?> value="Hot">Hot</option>
+                        <option <?php echo ($category === 'Végétarienne') ? 'selected' : ''; ?> value="Végétarienne">Végétarienne</option>
+                    </select>
+                    <?php if (isset($errors['category'])) {
+                        echo '<div class="invalid-feedback">';
+                            echo $errors['category'];
+                        echo '</div>';
+                    } ?>
+                </div>
+                <div class="form-group">
+                    <label for="description">Description :</label>
+                    <textarea name="description" id="description" rows="5" class="form-control <?php echo isset($errors['description']) ? 'is-invalid' : null; ?>"><?php echo $description; ?></textarea>
+                    <?php if (isset($errors['description'])) {
+                        echo '<div class="invalid-feedback">';
+                            echo $errors['description'];
+                        echo '</div>';
+                    } ?>
+                </div>
             </div>
         </div>
-    </main>
-
-
+        <div class="text-center">
+            <button class="btn btn-lg btn-block btn-danger text-uppercase font-weight-bold">Ajouter</button>
+        </div>
+    </form>
+</main>
 
 <?php
 // Le fichier footer.php est inclus sur la page
